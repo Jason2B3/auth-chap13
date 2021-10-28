a6f4b61f5919a4012b8c9def875b835ec329aefe
jason <2B3>
2021-10-27 22:56:47 -0400

    commit n225: Client-side Page Guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,12 +1,26 @@
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
-import { useSession, getSession } from "next-auth/client";
+import React, { useEffect, useState } from "react";
+import { getSession } from "next-auth/client";
 
 export default function UserProfile() {
-  const [session, loading] = useSession();
-  console.log("session:", session);
-  console.log("loading:", loading);
-  if (loading) return <p className={classes.profile}>Loading...</p>;
+  // Create/manage your own loading and session states
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedSession, setLoadedSession] = useState(null);
+  console.log(isLoading, loadedSession);
+  useEffect(() => {
+    const confirmSession = async function () {
+      const session = await getSession(); // falsy if we aren't logged in
+      setLoadedSession(session); // update our session state
+      // If getSession returns a falsy, we must not be logged in
+      // In this case, redirect the visitor to to the /auth path for authentication
+      if (!session) window.location.href = "/auth";
+      else setIsLoading(false); // update our loading state (we're done now)
+    };
+    confirmSession();
+  }, []);
+
+  if (isLoading) return <p className={classes.profile}>Loading...</p>;
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
@@ -14,5 +28,3 @@ export default function UserProfile() {
     </section>
   );
 }
-
-
