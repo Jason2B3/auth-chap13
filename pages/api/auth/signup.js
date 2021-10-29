a5029a1b1a0baaaa50cf10314bf8bf0c3b9a18b5
jason <2B3>
2021-10-28 23:39:16 -0400

    removed encrypt misinformation

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,11 +1,11 @@
 import { connectToDatabase } from "../../../helpers/db";
-import { encrypt } from "../../../helpers/auth";
+import { hashPW } from "../../../helpers/auth";
 //# API Route file
 export default async function handler(req, res) {
   if ((req.method = "POST")) {
     // Extract data from the request body
     const { email, password } = req.body;
-    const hashedPassword = await encrypt(password); // encrypt the password before storage
+    const hashedPassword = await hashPW(password); // hash the password before storage
     // Access db instance
     const client = await connectToDatabase();
     const db = client.db();
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
     // Enter the "users" collection to make a new account doc
     const result = await db.collection("users").insertOne({
       email,
-      password: hashedPassword, // is encrypted before insertion
+      password: hashedPassword, // is hashed before insertion
     });
     client.close();
     res.status(201).json({ message: "Created user!" });
